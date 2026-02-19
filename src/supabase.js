21190eb9a7333076ca1230d51691d28acf86da8d
西里博太 <hirota.nishizato@MacBook-Air-2.local>
2026-02-19 23:12:45 +0900

    fix supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://nmvgyxspomshuwiyihysq.supabase.co'
+const supabaseUrl = 'https://nmvgyxspomshuwiyhysq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tdmd5eHNwb21zaHV3aXloeXNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0OTY1MTUsImV4cCI6MjA4NzA3MjUxNX0.4oXJZYBoLovjYfB_JYdEWKJR7EoUoyqnuCQI-gpRCeI'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
